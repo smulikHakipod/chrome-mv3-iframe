0ed2dd56c15bb8d4b409041cae6b06a8081bb165
Yaron Shani <yaron.shani@gmail.com>
2022-06-09 16:26:51 +0300

    init

---
+++ content.js
@@ -0,0 +1,8 @@
+const iframe = document.createElement('iframe');
+iframe.setAttribute('src', chrome.runtime.getURL('iframe.html'));
+iframe.style.height = '300px';
+iframe.style.width = '300px';
+iframe.style.position = 'fixed';
+iframe.style.top = '0';
+
+document.body.appendChild(iframe);
